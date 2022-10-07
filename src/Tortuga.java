f07eaa1fa52e22ca91f8a86fc5e2f279cf95e7c7
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-10-06 20:52:49 -0700

    Carrera Tortuga - Liebre

--- src/Tortuga.java
+++ src/Tortuga.java
@@ -1,39 +1,73 @@
-public class Tortuga {
-    private int position;
-    private boolean winner = false;
-    public Tortuga(int position){
-        this.position = position;
+public class Tortuga extends Thread {
+    char[] pista;
+
+    public Tortuga(char[] a) {
+        this.pista = a;
     }
 
-    public synchronized void avanceRapido() throws InterruptedException{
-        // Esperar un segundo y ejecutar accion
-        Thread.sleep(1000);
-        position = position + 3;
-        notifyAll();
-    }
-    public synchronized void resbalon() throws InterruptedException{
-        // Esperar un segundo y ejecutar accion
-        Thread.sleep(1000);
-        position = position - 6;
-        if(position < 1){
-            position = 1;
+    public void run() {
+        boolean ganador = false;
+        int posicion = 0;
+
+
+        while (ganador == false) {
+            System.out.println("");
+            int pasos = (int) Math.floor(Math.random() * (100) + 1);
+
+            //Inicio avance rápido
+            if (pasos <= 50) {
+                if ((posicion + 6) > 69) { //Comprobar que la nueva posición no exceda el tamaño
+                    posicion = 69;
+                } else {
+                    pista[posicion] = 0;
+                    posicion = posicion + 6;
+                    pista[posicion] = 'T'; //Guardar nueva posición
+                }
+                Posiciones(pista);
+                Victoria(posicion, ganador); //Comprobar victoria
+            }
+            //Fin avance rápido
+            //Inicio resbalo
+            if (pasos > 50 && pasos <= 70) {
+                pista[posicion] = 0;
+                if ((posicion - 6) < 0) { //Comprobar que el descenso no sea menor a 0 (1 si tomamos el tamaño)
+                    posicion = 0;
+                } else {
+                    posicion = posicion - 6;
+                }
+                Posiciones(pista);
+            }
+            //Fin resbalo
+            //Inicio avance lento
+            if (pasos > 70 && pasos <= 100) {
+                if ((posicion + 1) > 69){
+                    posicion = 69;
+                } else {
+                    pista[posicion] = 0;
+                    posicion = posicion + 1;
+                    pista[posicion] = 'T'; //Imprimpir nueva posición
+                }
+                Posiciones(pista);
+                Victoria(posicion, ganador); //Comprobar victoria
+            }
         }
-        notifyAll();
     }
-    public synchronized void avanceLento() throws InterruptedException{
-        // Esperar un segundo y ejecutar accion
-        Thread.sleep(1000);
-        position = position + 3;
-        notifyAll();
-    }
-    public synchronized int get() {
-        notifyAll();
-        return position;
-    }
-    public synchronized boolean getWinner(){
-        if(position >= 70){
-            winner = true;
+
+    public static void Posiciones(char[] pista) {
+        for (int i = 0; i < pista.length; i++) {
+            if (pista[i] == 0) {
+                System.out.print(" ");
+            } else {
+                System.out.print(pista[i]);
+            }
         }
-        return winner;
+    }
+
+    public static boolean Victoria(int posicion, boolean ganador) {
+        if (posicion == 70) {
+            ganador = true;
+            System.out.printf("La liebre es la ganadora ");
+        }
+        return ganador;
     }
 }
